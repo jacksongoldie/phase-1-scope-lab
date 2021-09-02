4602eaac83c47048bc3e0074c1f7cb3db3e00607
Goldie <goldiejackson25@yahoo.com>
2021-09-01 19:00:15 -0500

    tests passing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+   return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'you';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'me';
+    return leastFavoriteCustomer;
+}
